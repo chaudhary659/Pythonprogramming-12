18a88b118994e25b3d81a06edbabe83b7dd913e0
chaudhary659 <142832502+chaudhary659@users.noreply.github.com>
2023-08-29 21:07:54 +0530

    Add files via upload

---
+++ temperature conversaion.py
@@ -0,0 +1,15 @@
+#convert (deg)celcius to fharenheite 
+
+F=float(input("enter the value of fharenheite"))
+
+C=5/9*(F-32)
+
+print("the vule of (deg)celcius is",C)
+
+#convert fharenheite to (deg)celcius
+
+C=float(input("enter the value of (deg)celcius "))
+
+F=C*(9/5)+32
+
+print("the value of fharenheite is",F)
